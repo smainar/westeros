11afb551dfe1e716b55e9c0ed9b8603a00c8806c
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-06 11:56:30 -0600

    Final commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,4 +4,26 @@ class SearchController < ApplicationController
       facade: HouseMembersSearchFacade.new(params[:house])
     }
   end
+
+  def member_count
+    members.count
+  end
+
+  def members
+    conn = Faraday.new(url: "http://westerosapi.herokuapp.com/api/v1/house/#{@house}?#{ENV['WESTOROS-API-KEY']}") do |f|
+      f.params["api_key"] = ENV['WESTOROS-API-KEY']
+      f.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/api/v1/house/#{@house}?api_key=egg")
+
+    house_member_search_info = JSON.parse(response.body, symbolize_names: true)["data"][0]["attributes"]["members"]
+
+    house_member_search_info.map do |search_info|
+      Member.new(search_info)
+    end
+  end
+
+  private
+  attr_reader :house
 end
---
+++ app/models/member.rb
@@ -0,0 +1,8 @@
+class Member
+  attr_reader :id, :name
+  
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+  end
+end
